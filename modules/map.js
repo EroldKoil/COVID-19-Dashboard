96e2cc7b9c206916c064608adcd21e93766e13a8
Denis Korotkevich <volk.denis98@gmail.com>
2020-12-13 13:53:24 +0300

    feat: add popup block for country

--- modules/map.js
+++ modules/map.js
@@ -1,48 +1,67 @@
 class Map {
+
     constructor() {
-        this.dataCountry = [];
-        this.countries = ['belarus', 'russia', 'poland'];
-        this.southWest = L.latLng(-80.98155760646617, -190);
-        this.northEast = L.latLng(89.99346179538875, 190);
+        this.southWest = L.latLng(-80.98155760646617, -185);
+        this.northEast = L.latLng(89.99346179538875, 185);
         this.map = new L.Map('map', {
             center: [0, 0],
-            minZoom: 2,
             zoom: 2,
+            maxZoom: 9,
             maxBounds: L.latLngBounds(this.southWest, this.northEast),
         });
-        this.casesOptions= {
-            radius: 30000,
-            fill: true,
-            fillColor: '#F73F3F',
-            fillOpacity: 0.6,
-            color: '#b32424',
-        };
-        this.getPositionCountry();
     }
 
     renderMap() {
-        const tileLayer = new L.TileLayer('https://{s}.tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token=hjwgVWeFpBZKCpiJuubyxLQTlfaKby0nFconlrJRUxRilidPFenqyUvzELqTeEjR');
+        const tileLayer = new L.TileLayer('https://tile.jawg.io/6a531b2b-270b-4f38-af60-86177cec21ad/{z}/{x}/{y}.png?lang=en&access-token=hjwgVWeFpBZKCpiJuubyxLQTlfaKby0nFconlrJRUxRilidPFenqyUvzELqTeEjR', {
+            minZoom: 2,
+            attribution: '<a href=\\"https://www.jawg.io\\" target=\\"_blank\\">&copy; Jawg</a> - <a href=\\"https://www.openstreetmap.org\\" target=\\"_blank\\">&copy; OpenStreetMap</a>&nbsp;contributors',
+            updateInterval: 10,
+        });
         this.map.addLayer(tileLayer);
     }
 
-    renderCases() {
-        this.dataCountry.forEach((country)=>{
-            const casesCircle = L.circle([country.Lat, country.Lon], this.casesOptions);
-            this.map.addControl(casesCircle);
-        });
-    }
-
-    getPositionCountry() {
-        this.countries.forEach((country)=>{
-            this.getCountry(country).then(response => {
-                this.dataCountry.push(response[0]);
+    renderCases(data) {
+        for (let key in data) {
+            let percentOfCases = (data[key].TotalConfirmed / dashboard.worldInfo.TotalConfirmed) * 100;
+            const casesCircle = L.circleMarker([data[key].coords.lat, data[key].coords.lon], {
+                radius: percentOfCases <1? 4: percentOfCases,
+                fill: true,
+                fillColor: '#F73F3F',
+                fillOpacity: 0.7,
+                color: '#F73F3F',
+                opacity: 1,
+                width:5,
             });
-        });
+            casesCircle.bindTooltip(`<div class="map-toolTip" ><img class="map-toolTip-img" src="https://restcountries.eu/data/${data[key].flag}.svg"><span class="map-toolTip-text">${data[key].Country}</span></div>`).openTooltip();
+
+            casesCircle.on('mouseover',this.showMoreInfo.bind(this,data[key]));
+            casesCircle.on('mouseout',this.hideMoreInfo);
+
+            this.map.addControl(casesCircle);
+        }
     }
 
-    async getCountry(name) {
-        const result = await fetch(`https://api.covid19api.com/country/${name}/status/confirmed`);
-        return await result.json();
+    showMoreInfo(country) {
+        const blockInfoCountry = document.getElementById('popupBlock');
+        const flagCountry = document.querySelector('.popup-img');
+        const nameCountry = document.querySelector('.popup-name');
+        const valueCountry = document.querySelector('.popup-value');
+        const titleValueCountry = document.querySelector('.popup-value-title');
+        const populationCountry = document.querySelector('.popup-population');
+
+        flagCountry.src = `https://restcountries.eu/data/${country.flag}.svg`;
+        nameCountry.textContent = country.Country;
+        valueCountry.textContent =country.TotalConfirmed.toLocaleString();
+        titleValueCountry.textContent ='Cases: ';
+        populationCountry.textContent = country.population.toLocaleString();
+        blockInfoCountry.classList.add('show-popup-block');
+        blockInfoCountry.classList.remove('hide-popup-block');
+    }
+
+    hideMoreInfo () {
+        const blockInfoCountry = document.getElementById('popupBlock');
+        blockInfoCountry.classList.remove('show-popup-block');
+        blockInfoCountry.classList.add('hide-popup-block');
     }
 
     redrawMap() {
@@ -54,7 +73,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const map = new Map();
     map.renderMap();
 
-    setTimeout(()=>{
-        map.renderCases();
+    setTimeout(() => {
+        console.log(dashboard.allInfo);
+        map.renderCases(dashboard.allInfo);
     }, 1500);
 });
+
+
+/*<div id="popupBlock" class="hide-popup-block">
+        <div class="popup-country"><span class="popup-name"></span></div>
+        <div><span class="popup-population-title">Population: </span><span class="popup-population"></span></div>
+        <div><span class="popup-value-title"></span><span class="popup-value"></span></div>
+        <img class="popup-img">
+      </div>*/
